fbb00203855045230ffb9fdb6920b0ba4aa213f4
sunitha.app <sunitha.app@gmail.com>
2020-10-12 22:02:04 +0200

    First auto deployment test

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello! This is an autodeployment test :)
+        </h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
